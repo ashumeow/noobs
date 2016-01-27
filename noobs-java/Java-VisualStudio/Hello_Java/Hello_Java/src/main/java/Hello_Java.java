4ad4c4de795afe26bbc187fcd5d93f67812a2fb4
ashumeow <ashumeow@live.com>
2016-01-27 12:26:45 +0530

    update

---
+++ noobs-java/Java-VisualStudio/Hello_Java/Hello_Java/src/main/java/Hello_Java.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Hello_Java {
+	public static void main(String[] args) {
+	// Testing Java in Visual Studio 2015 environment ;)
+		System.out.println("Hello Java!");
+	}
+}
