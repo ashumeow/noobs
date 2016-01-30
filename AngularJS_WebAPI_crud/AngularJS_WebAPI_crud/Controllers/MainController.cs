c49385f61bccd63aee7523cb815c8b09c79bf4ff
ashumeow <ashumeow@live.com>
2016-01-30 18:03:52 +0530

    push app

---
+++ AngularJS_WebAPI_crud/AngularJS_WebAPI_crud/Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularJS_WebAPI_crud.Controllers
+{
+    public class MainController : Controller
+    {
+        /// <summary>
+        /// This maps to the Main/Index.cshtml file.  This file is the main view for the application.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AngularJS_WebAPI_crud/AngularJS_WebAPI_crud/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AngularJS_WebAPI_crud.Startup))]
+
+namespace AngularJS_WebAPI_crud
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
